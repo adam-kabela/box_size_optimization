32e3295829a2ecb50b69d17670ffe711e9f92557
adam-kabela <adam.kabela@gmail.com>
2023-09-25 00:06:32 +0200

    Added more evaluations and visualizations.

--- main.py
+++ main.py
@@ -43,43 +43,85 @@ dataset = pandas.read_csv(data, sep=';')
 #get_basic_idea(dataset) # get basic idea about the data, please comment out if not needed
 orders = prepare_data(dataset)
 
-#combine best orthogonal containers and heuristic containers for all rotations
+# combine best orthogonal containers and heuristic containers for all rotations
 best_containers_and_layouts = prepare_best_containers(orders, trials)
+best_containers_and_layouts.to_csv('evaluations/data_used_for_evaluation/best_containers_and_layouts.csv', sep=';')
 
-#evaluate expert judgement boxes
+# evaluate expert judgement boxes
 evaluation = evaluate_box_choice(best_containers_and_layouts, expert_judgement_boxes)
 output_evaluation_metrics(evaluation, expert_judgement_boxes)
-evaluation.to_csv('results/expert_judgement_boxes_evaluation_data.csv', sep=';')
+evaluation.to_csv('evaluations/data_used_for_evaluation/expert_judgement_boxes_evaluation_data.csv', sep=';')
 
-#also evaluate individual boxes from expert judgement boxes
+# also evaluate individual boxes from expert judgement boxes
 for box in expert_judgement_boxes:
     evaluation = evaluate_box_choice(best_containers_and_layouts, [box])
     output_evaluation_metrics(evaluation, [box])
 
-#message time check file
-
+# expert judgement boxes contain one larger box and one slammer box, it seems like a reasonable strategy
+# intuitively larger box affects packability, smaller box improves free space percentage 
+# first evaluate candidates for the larger box
 matrix_size = len(available_box_sizes)
 orders_packed_percentages = numpy.zeros((matrix_size, matrix_size))
+free_space_percentages = numpy.zeros((matrix_size, matrix_size))
+free_space_percentages_for_good_boxes = numpy.zeros((matrix_size, matrix_size))
 for i in range(matrix_size):
     for j in range(matrix_size):
         box = (available_box_sizes[i], available_box_sizes[j])
         evaluation = evaluate_box_choice(best_containers_and_layouts, [box])
         orders_packed_percentages[i][j] = packable_orders_percentage(evaluation)
+        free_space_percentages[i][j] = None
+        if orders_packed_percentages[i][j] > 0:
+            free_space_percentages[i][j] = box_free_space_percentage(evaluation)
+        free_space_percentages_for_good_boxes[i][j] = None
+        if orders_packed_percentages[i][j] >= 60:
+            free_space_percentages_for_good_boxes[i][j] = free_space_percentages[i][j]
         
-caption = 'Percentages of packed orders for boxes of all lengths and widths'
-output_file = 'all_boxes_evaluated'
-output_and_wisualize(orders_packed_percentages, available_box_sizes, available_box_sizes, 'RdYlGn', caption, output_file)
+caption = 'Percentages of packed orders for boxes of all lengths and widths (green = majority of orders packed)'
+output_file = 'all_boxes_evaluated_packed_orders'
+output_and_visualize(orders_packed_percentages, available_box_sizes, available_box_sizes, 'RdYlGn', caption, output_file)
 
-#hero task
-#intuitively larger box affects packability, smaller affects free space 
-#check boxes by combinations of sizes of best boxes
-#output to csv
+caption = 'Percentages of free box space over all packed orders for boxes of all lengths and widths (light gray background = majority of box space is free )'
+output_file = 'all_boxes_evaluated_free_space'
+output_and_visualize(free_space_percentages, available_box_sizes, available_box_sizes, 'gray', caption, output_file)
 
-#output used layout
-#plot layout
+caption = 'Percentages of free box space over all packed orders for boxes that cover > 60% orders (light gray background = most of box space is free )'
+output_file = 'larger_boxes_evaluated_free_space'
+output_and_visualize(free_space_percentages_for_good_boxes, available_box_sizes, available_box_sizes, 'gray', caption, output_file)
 
-#logging
-#log time
+all_possible_boxes = []
+for i in range(matrix_size):
+    for j in range(i+1):
+        box = (available_box_sizes[i], available_box_sizes[j])
+        all_possible_boxes.append(box)
 
-#smart angles
-#smart grid
+best_boxes = []
+best_boxes_pack = 0
+best_boxes_free_space = 100
+for i in range(len(all_possible_boxes)):
+    for j in range(i):
+        larger_box = all_possible_boxes[i]
+        smaller_box = all_possible_boxes[j]
+        boxes = [larger_box, smaller_box]
+        evaluation = evaluate_box_choice(best_containers_and_layouts, boxes)
+        orders_packed = packable_orders_percentage(evaluation)
+        if orders_packed >= (60 + 5): #buffer
+            free_space = box_free_space_percentage(evaluation)    
+            if free_space < best_boxes_free_space:
+                best_boxes = boxes
+                best_boxes_pack = orders_packed
+                best_boxes_free_space = free_space
+                
+print(best_boxes, best_boxes_pack, best_free_space)        
+
+#message time check file
+
+#superhero hero task
+#check all pairs of boxes 
+
+#output used layout for each order and suitable box
+#plot layout of items in the box
+
+#add logging
+
+#be smarter on rotation angles
+#be smarter on discarding points of orthogonal grid
--- output_visualisation.py
+++ output_visualisation.py
@@ -2,21 +2,23 @@ import numpy
 from IPython.display import HTML
 from group_orders import *
 
-def output_and_wisualize(matrix, column_names, row_names, color_scheme, caption, output_file):
+def output_and_visualize(matrix, column_names, row_names, color_scheme, caption, output_file):
     table = pandas.DataFrame(matrix)
     table.columns = column_names
     table.index = row_names
-    table.to_csv('results/' + output_file + '.csv', sep=';')
+    table.to_csv('evaluations/results_csv/' + output_file + '.csv', sep=';')
     
     styler = table.style
     styler.format('{:.2f}')
-    #styler = styler.background_gradient(axis=None, low=0.75, high=1.0)
     styler = styler.background_gradient(axis=None, cmap=color_scheme)
+    styler.applymap(lambda x: 'color: transparent' if pandas.isnull(x) else '')
+    styler.applymap(lambda x: 'background-color: white' if pandas.isnull(x) else '')
     styler.set_caption(caption)
-    #apply(highlight_max)\
+    #styler.applymap(lambda x: 'color: transparent' if pandas.isnull(x) else '')
+    #apply(highlight_max) #this could be nice
     
     html = styler.to_html()
     
-    text_file = open('results/' + output_file + '.html', 'w')
+    text_file = open('evaluations/results_html/' + output_file + '.html', 'w')
     text_file.write(html)
     text_file.close()
